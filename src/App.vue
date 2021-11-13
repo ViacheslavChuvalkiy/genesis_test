9c7b328f630befc81f9cad2bb6e2273369223340
Viacheslav Chuvalskyi <slavchuval@gmail.com>
2021-11-13 21:44:27 +0200

    init the project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
-    <Content/>
   </div>
 </template>
 
 <script>
-  import Content from "./components/organisms/Content.vue";
-  import { mapMutations, mapActions } from "vuex";
 
 export default {
-  components: {
-    Content,
-  },
-  methods: {
-    ...mapMutations(["getPageViewFromLocalStorage"]),
-    ...mapActions(['getVideoList'])
-  },
-  mounted() {
-    if (localStorage.tasksList) {
-      this.getPageViewFromLocalStorage();
-    }
-    this.getVideoList();
-  },
 }
 </script>
 
 <style>
-
   #app{
     margin: 0;
     padding: 0;
